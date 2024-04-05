8ae13571eb66ab5f6940aae45f9a89ad761ee2f8
pushpinder09 <101087908+pushpinder09@users.noreply.github.com>
2024-04-05 14:10:17 +0530

    Add files via upload
    Introduction to UI Kit

---
+++ 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+/*:
+## Exercise - Create Functions
+
+ Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
+ */
+func introducemyself()
+{
+    print("Hi my name is Pushpinder Thakur and i m currently studying Ios")
+}
+
+introducemyself()
+
+//:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
+import Foundation
+func magicEighyBall(){
+    let randomNum = Int.random(in : 0...4)
+    switch randomNum{
+    case 0:
+        print("zero")
+        
+    case 1:
+        
+        print("one")
+        
+    case 2:
+        
+        print("two")
+        
+    case 3:
+        
+        print("three")
+        
+    case 4:
+        
+        print("four")
+        
+    default:
+        print("out of range")
+    }
+}
+magicEighyBall()
+
+/*:
+page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
+ */
