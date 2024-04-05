ce6af1f1a4a7e2254c2df182a8c735a273156b26
pushpinder09 <101087908+pushpinder09@users.noreply.github.com>
2024-04-05 13:58:56 +0530

    Add files via upload
    Getting Started

---
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+/*:
+## Exercise - Constants
+
+ Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
+ */
+let friends = 100;
+print(friends)
+
+//:  Now assume you go through and remove friends that aren't active on social media. Attempt to update your `friends` constant to a lower number than it currently is. Observe what happens and then move to the next step.
+//friends = 1;
+
+//:  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
+print("The value can not be printed because fridnds is mutable and thus valued can not be modified")
+
+/*:
+page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
+ */
