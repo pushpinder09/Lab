ccc097b1cde82080d5921c736d33a9906cd9a7bd
pushpinder09 <101087908+pushpinder09@users.noreply.github.com>
2024-04-06 08:53:27 +0530

    Add files via upload
    App Anatomy

---
+++ App Anatomy/App Anatomy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Anatomy
+//
+//  Created by Student on 01/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ App Anatomy/App Anatomy/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  App Anatomy
+//
+//  Created by Student on 01/04/24.
+//
+
+import UIKit
+print("This must run first");
+UIApplicationMain(CommandLine.argc,CommandLine.unsafeArgv,nil,NSStringFromClass(AppDelegate.self))
